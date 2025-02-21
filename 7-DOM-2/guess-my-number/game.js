90cf584f12f05ccc54a28a24d2420a87207ec18a
MH020 <166133291+MH020@users.noreply.github.com>
2025-02-21 09:10:26 +0100

    opgaver

--- 7-DOM-2/guess-my-number/game.js
+++ 7-DOM-2/guess-my-number/game.js
@@ -1,7 +1,24 @@
 //Save the buttons as constants
-
+const guessButton = document.querySelector("#guessButton")
 //Add an eventListener that takes input from the input field
+const inputfield = document.querySelector("#userGuess")
+const text = document.querySelector("p")
+const reset = document.querySelector("#reset")
+
+
 //Print something to the console
+guessButton.addEventListener("click",() => {
+    let guessvalue = Number(inputfield.value)
+    console.log("click")
+    if (guessvalue === randomNumber) {text.innerText =("right")}
+    if (guessvalue > randomNumber) {text.innerText =("it should be higher")}
+    if(guessvalue < randomNumber) {text.innerText =("it should be lower")}
+})
+reset.addEventListener("click",() => {
+    randomNumber = generateNumber();
+    console.log(randomNumber)
+})
+
 
 //Make a random number between 1-5
 let randomNumber = generateNumber();
--- 7-DOM-2/single-button/script.js
+++ 7-DOM-2/single-button/script.js
@@ -4,6 +4,10 @@ const button = document.querySelector("#spinButton");
 
 const testButton = document.querySelector("#test");
 
+const reset = document.querySelector("#reset")
+
+const text = document.querySelector("p")
+
 testButton.addEventListener("click", () =>{
     console.log("Hey du klikkede knappen!!!!");
 });
@@ -16,3 +20,21 @@ button.addEventListener("click", () => {
     //This spins around the button
     document.getElementById("spinButton").style.transform += "skew(-50deg, 25deg) rotate(270deg)";
 });
+
+const clickme = document.querySelector("#click-me");
+
+clickme.addEventListener("click", () => console.log("ah thank you") )
+
+button.addEventListener("click", () => {
+    timesPressed++
+    if (timesPressed > 1)
+    text.innerText = ("Please do not do that again")
+    if (timesPressed > 2) text.innerText = ("I told you not to do that")
+    if (timesPressed > 3) text.innerText = ("STOP!!!")
+});
+
+reset.addEventListener("click", () => timesPressed = 0)
+
+
+
+
