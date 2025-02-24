89811eb25c89d29cdb5280e550f09c2a978e198e
MH020 <166133291+MH020@users.noreply.github.com>
2025-02-24 09:29:07 +0100

    dasdsad

--- 8-fetch/basic-fetch-GET/index.js
+++ 8-fetch/basic-fetch-GET/index.js
@@ -1,7 +1,33 @@
+const pElement = document.createElement("p")
+document.body.appendChild(pElement)
+
 fetch("https://catfact.ninja/fact")
     .then(response =>{
         return response.json();
     })
     .then(dataAsJSON =>{
         console.log(dataAsJSON);
+       // pElement.innerText = dataAsJSON.fact;
+
+    });
+
+fetch("https://catfact.ninja/facts")
+    .then(response =>{
+        return response.json();
+    })
+    .then(dataAsJSON =>{
+        console.log(dataAsJSON);
+        pElement.innerText = dataAsJSON.data[0].fact;
+
+        for (let i = 0; i < dataAsJSON.data.length; i++) {
+            let longestFact = 0;
+            let index = 0;
+            if  (dataAsJSON.data[i].length > longestFact){
+                index = i;
+            }
+            pElement.innerText = dataAsJSON.data[index].fact;
+
+        }
+
+
     });
--- 8-fetch/modal/cat-facts-modal.js
+++ 8-fetch/modal/cat-facts-modal.js
@@ -1,1 +1,49 @@
+const modal = document.querySelector("#factModal");
+
+const moreFactsButton = document.querySelector("#click");
+
+const closeModel = document.querySelector(".close-modal")
+
+const modalTitle = document.querySelector("#modalTitle")
+
+const modalText = document.querySelector("#modalContent")
+
+const moreInfo = document.querySelectorAll(".more-info")
+
+const catFacts = document.querySelectorAll("p")
+
+
+
+moreFactsButton.addEventListener("click", () => {
+    modal.className = "modalShown";
+    modalTitle.innerText = "more facts soon now leave me alone";
+    modalText.innerText = "We currently do not have any more facts. Our engineer staff is working hard to provide more relevant fun facts."
+});
+closeModel.addEventListener("click", () =>{modal.className = "modal"})
+
+
+moreInfo.forEach(button = () => {
+    button.addEventListener("click", () => { modal.className = "modalShown"
+    const factContent = event.target.parentNode
+    const title = factContent.querySelector("h2").textContent;
+    const text = factContent.querySelector("p").textContent;
+
+    modalTitle.textContent = title;
+    modalText.textContent =text;
+
+    });
+});
+
+fetch("https://catfact.ninja/fact")
+    .then(response =>{
+        return response.json();
+    })
+    .then(dataAsJSON =>{
+        console.log(dataAsJSON);
+        //We can work with the data as JSON in here
+        alert(dataAsJSON.fact)
+        catFacts.forEach()
+    });
+
+
 
