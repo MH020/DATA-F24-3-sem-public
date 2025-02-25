f73252a9990c2223a33e9f5f01f9b7756358b5dd
dean <nicklasfrederiksen@gmail.com>
2025-02-25 13:40:30 +0100

    first

--- 9-fetch-2-async-POST/error-handling/index.js
+++ 9-fetch-2-async-POST/error-handling/index.js
@@ -1,15 +1,16 @@
-fetch("https://swapi.dev/api/people/10")
-    .then(dataStream => dataStream.json(), () => console.log("error"))
-    .then(json => console.log(json))
-    .catch(error => console.log("error"));
+// fetch("https://swapi.dev/api/people/100")
+//     .then(dataStream => dataStream.json(), () => console.log("error"))
+//     .then(json => console.log(json))
+//     .catch(error => console.log("error"));
 
 
-// fetch('https://swapi.dev/api/people/100').then(function(response) {
-//     if (!response.ok) {
-//         throw new Error("An error has occurred");
-//     } else {
-//         //Ok response
-//     }
-// }).catch(function(err) {
-//     console.log("An error has occurred");
-// });
+fetch('https://swapi.dev/api/people/100')
+    .then((response) => {
+    if (!response.ok) {
+        throw new Error("An error has occurred");
+    } else {
+        //Ok response
+    }
+}).catch(function(err) {
+    console.log("An error has occurred");
+});
